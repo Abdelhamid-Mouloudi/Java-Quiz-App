f8b3e6a1cff1f1550ab811b2c8b8bc4d723b6b00
Abdelhamid-Mouloud <abdelhamidmouloudi2003@gmail.com>
2024-10-14 23:54:30 +0100

    service discovery initial

---
+++ Microservices architecture/quiz-service/src/main/java/com/abdelhamid/quiz_service/dao/QuizDao.java
@@ -0,0 +1,9 @@
+package com.abdelhamid.quiz_service.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abdelhamid.quiz_service.model.Quiz;
+
+public interface QuizDao extends JpaRepository<Quiz,Integer>{
+
+}
