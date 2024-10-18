5fae9779c48168493b3ae514dec20343ea5528a5
Abdelhamid-Mouloud <abdelhamidmouloudi2003@gmail.com>
2024-10-18 09:58:56 +0100

    setting up feign

---
+++ Microservices architecture/quiz-service/src/main/java/com/abdelhamid/quiz_service/feign/QuizInterface.java
@@ -0,0 +1,29 @@
+package com.abdelhamid.quiz_service.feign;
+
+import com.abdelhamid.quiz_service.model.QuestionWrapper;
+import com.abdelhamid.quiz_service.model.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FeignClient("QUESTION-SERVICE")
+public interface QuizInterface {
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) ;
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) ;
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(List<Response> responses) ;
+
+
+}
+
+
+
